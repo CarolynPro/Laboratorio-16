d56295290449f374b826b405168f8bffaede8b19
CarolynPro <carol.ingabil.18@gmail.com>
2025-04-20 22:01:16 -0500

    Estilos

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function validarFormulario(){
-    let nombre=document.getElementById('nombre').value;
-    let email=document.getElementById('email').value;
-    let imagen=document.getElementById('imagen').value;
+    let nombre=document.getElementById("nombre").value;
+    let email=document.getElementById("email").value;
+    let imagen=document.getElementById("imagen").value;
     if (nombre=="" || email=="" || imagen==""){
         alerta("Todos los campos son obligatorios...");
         return false;
